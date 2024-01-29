5ec103192df4bc5e8c04a69238a395b7b2981d9a
RoyalR4z <viniciusviana2004@hotmail.com>
2024-01-29 12:44:26 -0300

    :sparkles: feat: Adding the Put Method to the Category API

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -94,6 +94,38 @@ namespace backendCsharp.Controllers {
             
         }
 
+        [HttpPut("{id}")]
+        public ActionResult<List<CategoryModel>> PutById([FromBody] dynamic dadosObtidos, int id) {
+
+            // Obter o cabeçalho "Authorization" da solicitação
+            string? authorizationHeader = HttpContext.Request.Headers["Authorization"];
+
+            string token = "";
+
+            try {
+                // Remover o prefixo "Bearer " para obter apenas o token
+                token = authorizationHeader?.Substring("Bearer ".Length).Trim() ?? "";
+            } catch {
+                token = authorizationHeader ?? ""; 
+            }
+
+            Validate validator = new Validate();
+
+            try {
+
+                if (validator.ValidatingToken(token ?? "", true)) {
+
+                    return Ok(AlterarCategoria(id, dadosObtidos));
+                } else {
+
+                    return Ok("Unauthorized");
+                }
+
+            } catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
         private dynamic ConsultarCategorias(int? id) {
 
             Enviro env = new Enviro();
@@ -215,5 +247,49 @@ namespace backendCsharp.Controllers {
                 connection.Close();
             }
         }
+
+        private IActionResult AlterarCategoria(int id, dynamic dadosObtidos) {
+    
+            Validate validator = new Validate();
+            Enviro env = new Enviro();
+
+            // Convertendo os Dados Obtidos para JSON
+            string jsonString = System.Text.Json.JsonSerializer.Serialize(dadosObtidos);
+            CategoryModel? dados = JsonConvert.DeserializeObject<CategoryModel>(jsonString);
+
+            string nome = dados?.Name ?? "";
+            string subtitle = dados?.Subtitle ?? "";
+
+            validator.existsOrError(nome, @"Nome não informado");
+            validator.existsOrError(subtitle, @"Informe a Descrição");
+
+            using (NpgsqlConnection  connection = new NpgsqlConnection(env.ObtendoConfig())) {
+                connection.Open();
+
+                string query = "UPDATE category SET name = @Name, subtitle = @Subtitle WHERE id = @Id;";
+
+                // Crie um comando SQL com a query e a conexão
+                using (NpgsqlCommand command = new NpgsqlCommand(query, connection)) {
+
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@Name", nome);
+                    command.Parameters.AddWithValue("@Subtitle", subtitle);
+
+                    // Execute o comando
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    connection.Close();
+
+                    // Verifique se alguma linha foi afetada (deve ser maior que 0)
+                    if (rowsAffected > 0) {
+                        return null;
+                    } else {
+                        return StatusCode(500, "Falha ao Alterar dados.");
+                    }
+                }
+
+            }
+        }
+
     }
 }
